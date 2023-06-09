ac9b0816445098b0521b22a5fd558332a8557d25
sigiriyz <dima170490@mail.ru>
2023-06-09 14:09:49 +1000

    sum random

--- example003/Program.cs
+++ example003/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random() .Next(1,50);//рандом от 1 до 49
+Console.WriteLine(numberA);
+int numberB = new Random() .Next(1,50);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
